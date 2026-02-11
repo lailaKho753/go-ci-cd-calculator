9ccc63acce76b9f1a5453b67b8d02b6c5b16a64e
LailaKho <laila99@mail.ugm.ac.id>
2026-02-11 23:02:24 +0700

    initial commit: basic calculator API

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lailaKho753/go-ci-cd-calculator.git
+
+go 1.21.3
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Request struct {
+	A int `json:"a"`
+	B int `json:"b"`
+}
+
+type Response struct {
+	Result int `json:"result"`
+}
+
+func addHandler(w http.ResponseWriter, r *http.Request) {
+	var req Request
+	json.NewDecoder(r.Body).Decode(&req)
+
+	res := Response{
+		Result: req.A + req.B,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(res)
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("OK"))
+}
+
+func main() {
+	http.HandleFunc("/health", healthHandler)
+	http.HandleFunc("/add", addHandler)
+
+	http.ListenAndServe(":8080", nil)
+}
